d0d27241231483518e876ab93d0482dac6d2632d
C-D-Lewis <Bonsitm@gmail.com>
2025-02-18 21:31:27 +0000

    news-headlines backend refresh

--- watchapps/news-headlines/backend/src/modules/data.js
+++ watchapps/news-headlines/backend/src/modules/data.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-param-reassign */
 const fetch = require('node-fetch');
-const timelinejs = require('pebble-timeline-js-node');
+// const timelinejs = require('pebble-timeline-js-node');
 
-const {
-  API_KEY_PROD,
-  API_KEY_SANDBOX,
-} = process.env;
+const { API_KEY_PROD, API_KEY_SANDBOX } = process.env;
 
-const MAX_PUSHED = 1;  // Max pins pushed each INTERVAL. Prevents infamous timeline blob db errors.
-const MAX_DUPLICATES = 50;  // Max 'already pushed' stories
+const API_URL_ROOT = 'https://timeline-api.rebble.io/';
+const MAX_PUSHED = 1; // Max pins pushed each INTERVAL. Prevents infamous timeline blob db errors.
+const MAX_DUPLICATES = 50; // Max 'already pushed' stories
 
-const dupeBuffer = [];  // Check new stories against the last MAX_DUPLICATES to prevent hour-later duplucates
-let cacheFirst = false;  // Don't post pins right away - stateful
+// Check new stories against the last MAX_DUPLICATES to prevent hour-later duplucates
+const dupeBuffer = [];
+// Don't post pins right away - stateful
+let cached = true;
 
 /**
  * Extract text from a larger block with locator strings.
@@ -40,33 +41,37 @@ const extract = (text, befores, after) => {
  * @param {string} str - String to decode.
  * @returns {string} Decoded string.
  */
-const decode = (str) => {
-  str = str.split(/&amp;/g).join('&');
-  str = str.split('<![CDATA[').join('');
-  return str.split(']]>').join('');
-}
+const decode = (str) => str
+  .split(/&amp;/g)
+  .join('&')
+  .split('<![CDATA[')
+  .join('')
+  .split(']]>')
+  .join('');
 
 /**
  * Get stories from an XML string.
  *
- * @param {string} xml- XML string to extract stories from.
+ * @param {string} xml - XML string to extract stories from.
  * @returns {Array} Extracted stories.
  */
 const getStories = (xml) => {
   const items = [];
   xml = xml.split('<item>');
   xml.shift();
-  xml.map((xmlChunk) => {
+  xml.forEach((xmlChunk) => {
     const story = {
       title: decode(extract(xmlChunk, ['<title>'], '</title>')),
-      description: decode(extract(xmlChunk, ['<description>'], '</description>')),
-      date: extract(xmlChunk, ['<pubDate>'], '</pubDate>')
+      description: decode(
+        extract(xmlChunk, ['<description>'], '</description>'),
+      ),
+      date: extract(xmlChunk, ['<pubDate>'], '</pubDate>'),
     };
 
-    if(!dupeBuffer.find((dupe) => dupe.title === story.title)) {
+    if (!dupeBuffer.find((dupe) => dupe.title === story.title)) {
       dupeBuffer.unshift(story);
-      if(dupeBuffer.length > MAX_DUPLICATES) dupeBuffer.pop();
-     
+      if (dupeBuffer.length > MAX_DUPLICATES) dupeBuffer.pop();
+
       items.push(story);
       console.log(`Added new story: \n${story.title}\n${story.description}\n`);
     }
@@ -78,15 +83,43 @@ const getStories = (xml) => {
 };
 
 /**
+ * Insert a pin into the timeline, user (non-shared mode)
+ *
+ * @param {object} pin - Pin to insert.
+ */
+const insertUserPin = async (pin) => {
+  const url = `${API_URL_ROOT}v1/user/pins/${pin.id}`;
+
+  const res = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-User-Token': API_KEY_SANDBOX,
+    },
+    body: JSON.stringify(pin),
+  });
+
+  const body = await res.text();
+  if (body !== 'OK') {
+    console.log(`Failed to insert pin: ${body}`);
+    return;
+  }
+  console.log(`Inserted pin: ${pin.id}`);
+};
+
+/**
  * Push a pin to the timeline.
  *
  * @param {Array} stories - Array of stories to push.
  * @param {number} index - Index of the story to push.
  */
 const pushPin = (stories, index) => {
-  const pubDate = new Date(stories[index].date);
+  // const pubDate = new Date(stories[index].date);
+  const pubDate = new Date();
+  pubDate.setHours(pubDate.getHours() + 12);
+
   const pin = {
-    id: 'bbcnews-story-' + pubDate.getTime(),
+    id: `news-headlines-story-${pubDate.getTime()}`,
     time: pubDate.toISOString(),
     layout: {
       type: 'genericPin',
@@ -95,17 +128,19 @@ const pushPin = (stories, index) => {
       subtitle: 'BBC News',
       body: stories[index].description,
       foregroundColor: '#FFFFFF',
-      backgroundColor: '#AA0000'
-    }
+      backgroundColor: '#AA0000',
+    },
   };
   console.log(`pin=${JSON.stringify(pin, null, 2)}`);
+
+  // Add pin for this user
+  insertUserPin(pin);
 
   // FIXME: Rebble timeline API doesn't support shared topics yet
   // const TOPIC = 'headlines';
   // timelinejs.insertSharedPin(pin, [TOPIC], API_KEY_PROD, console.log);
-  // timelinejs.insertSharedPin(pin, [TOPIC], API_KEY_SANDBOX, console.log); 
-  // fcm.post('News Headlines', 'news_headlines__latest', `${pin.layout.title} - ${pin.layout.body}`);
-}
+  // timelinejs.insertSharedPin(pin, [TOPIC], API_KEY_SANDBOX, console.log);
+};
 
 /**
  * Download the BBC News RSS feed and push pins to the timeline.
@@ -113,17 +148,17 @@ const pushPin = (stories, index) => {
 const download = async () => {
   const res = await fetch('http://feeds.bbci.co.uk/news/rss.xml');
   const body = await res.text();
-  
-  if(cacheFirst) {
+
+  if (!cached) {
     console.log('Caching on first run');
-    cacheFirst = false;
+    cached = true;
     return;
   }
 
   const stories = getStories(body);
-  if(stories.length < 1) return;
+  if (stories.length < 1) return;
 
-  for(let i = 0; i < MAX_PUSHED; i++) pushPin(stories, i);
+  for (let i = 0; i < MAX_PUSHED; i += 1) pushPin(stories, i);
 };
 
 module.exports = { download };
--- watchapps/news-headlines/backend/src/node-common.js
+++ watchapps/news-headlines/backend/src/node-common.js
@@ -5,7 +5,7 @@ const path = `/home/${USER}/code/node-microservices`;
  * Import modules directly from the node-common project in this repo.
  *
  * @param {string[]} list - Array of modules names.
- * @returns {Object[]} Array of loaded modules.
+ * @returns {object[]} Array of loaded modules.
  */
 module.exports = (list) => list.reduce((acc, item) => ({
   ...acc,
