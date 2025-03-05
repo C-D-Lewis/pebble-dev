b93e64d42ab62987e7b57b8de74c81a8f0f18d39
C-D-Lewis <Bonsitm@gmail.com>
2025-03-05 09:45:21 +0000

    Add wristponder-redux pebble

---
+++ watchapps/wristponder-redux/pebble/src/common/contact_window.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <pebble.h>
+
+#include "../config.h"
+#include "../modules/data.h"
+
+#include "sub_window.h"
+#include "responses_window.h"
+
+void contact_window_push();
+
+void contact_window_reload_data();
---
+++ watchapps/wristponder-redux/pebble/src/common/sub_window.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <pebble.h>
+
+#include "../modules/data.h"
+
+#include "responses_window.h"
+
+void sub_window_push(ContactType type);
