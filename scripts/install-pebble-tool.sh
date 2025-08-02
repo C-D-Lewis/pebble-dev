655cd42fba36bce009a4955ee990520ecfed704e
C-D-Lewis <Bonsitm@gmail.com>
2025-08-02 18:24:03 +0100

    update

--- scripts/install-pebble-tool.sh
+++ scripts/install-pebble-tool.sh
@@ -4,9 +4,11 @@ set -e
 
 echo ">>> Installing dependencies"
 sudo apt-get update && sudo apt-get install -y \
-    curl \
-    libsdl1.2debian \
-    libfdt1
+  curl \
+  libsdl1.2debian \
+  libfdt1 \
+  python3.12-venv \
+  gcc-arm-none-eabi
 
 echo ">>> Installing uv"
 curl -LsSf https://astral.sh/uv/install.sh | sh
