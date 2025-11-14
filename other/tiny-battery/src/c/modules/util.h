4844d907015393fdc13ca3c42082b7e28cb60dc4
C-D-Lewis <Bonsitm@gmail.com>
2025-11-14 21:45:55 +0000

    Stat window

--- other/tiny-battery/src/c/modules/util.h
+++ other/tiny-battery/src/c/modules/util.h
@@ -2,6 +2,8 @@
 
 #include <pebble.h>
 
+#include "data.h"
+
 TextLayer* util_make_text_layer(GRect frame, GFont font);
 
 void util_fmt_time(int timestamp_s, char* buf, int buf_size);
--- other/tiny-battery/src/c/windows/stat_window.c
+++ other/tiny-battery/src/c/windows/stat_window.c
@@ -1,86 +1,87 @@
 #include "stat_window.h"
 
 static Window *s_window;
-static TextLayer *s_enabled_layer, *s_stats_layer, *s_history_layer;
+static StatusBarLayer *s_status_bar;
+static TextLayer *s_enabled_layer, *s_stats_layer, *s_history_layer;;
 
 static void update_data() {
-  // Is enabled?
   bool is_enabled = data_get_wakeup_id() != DATA_EMPTY;
-  static char s_enabled_buff[16];
-  snprintf(s_enabled_buff, sizeof(s_enabled_buff), "Enabled: %s", is_enabled ? "Yes" : "No");
-  text_layer_set_text(s_enabled_layer, s_enabled_buff);
-
   time_t wakeup_ts;
   wakeup_query(data_get_wakeup_id(), &wakeup_ts);
 
-  // Current stats
-  static char s_current_buff[128];
-  static char s_fmt_discharge_buff[8];
-  static char s_fmt_update_buff[8];
-  static char s_fmt_wakeup_buff[8];
-  util_fmt_time(data_get_discharge_start_time(), &s_fmt_discharge_buff[0], sizeof(s_fmt_discharge_buff));
-  util_fmt_time(data_get_last_update_time(), &s_fmt_update_buff[0], sizeof(s_fmt_update_buff));
-  util_fmt_time(wakeup_ts, &s_fmt_wakeup_buff[0], sizeof(s_fmt_wakeup_buff));
-  snprintf(
-    s_current_buff,
-    sizeof(s_current_buff),
-    "S: %s\nU: %s\nW: %s\nB: %d, P: %s",
-    &s_fmt_discharge_buff[0],
-    &s_fmt_update_buff[0],
-    &s_fmt_wakeup_buff[0],
-    data_get_last_charge_perc(),
-    data_get_was_plugged() ? "true": "false"
-  );
-  text_layer_set_text(s_stats_layer, s_current_buff);
+  static char s_enabled_buff[16];
+  snprintf(s_enabled_buff, sizeof(s_enabled_buff), "Enabled: %s", is_enabled ? "Yes": "No");
+  text_layer_set_text(s_enabled_layer, s_enabled_buff);
 
-  // Recent history
-  SampleData *sample_data = data_get_sample_data();
-  static char s_history_buff[64];
-  snprintf(
-    s_history_buff,
-    sizeof(s_history_buff),
-    "H: %d, %d, %d, %d, %d, %d\n-> avg: %d",
-    sample_data->history[0],
-    sample_data->history[1],
-    sample_data->history[2],
-    sample_data->history[3],
-    sample_data->history[4],
-    sample_data->history[5],
-    data_get_history_avg_rate()
-  );
-  text_layer_set_text(s_history_layer, s_history_buff);
+  text_layer_set_text(s_stats_layer, "");
+
+  if (is_enabled) {
+    // Current stats
+    static char s_values_buff[128];
+    static char s_fmt_discharge_buff[8];
+    static char s_fmt_update_buff[8];
+    static char s_fmt_wakeup_buff[8];
+    util_fmt_time(data_get_discharge_start_time(), &s_fmt_discharge_buff[0], sizeof(s_fmt_discharge_buff));
+    util_fmt_time(data_get_last_update_time(), &s_fmt_update_buff[0], sizeof(s_fmt_update_buff));
+    util_fmt_time(wakeup_ts, &s_fmt_wakeup_buff[0], sizeof(s_fmt_wakeup_buff));
+    snprintf(
+      s_values_buff,
+      sizeof(s_values_buff),
+      "Unplugged: %s\nLast sample: %s\nNext sample: %s\nLast value: %d\nWas plugged: %s",
+      &s_fmt_discharge_buff[0],
+      &s_fmt_update_buff[0],
+      &s_fmt_wakeup_buff[0],
+      data_get_last_charge_perc(),
+      data_get_was_plugged() ? "true": "false"
+    );
+    text_layer_set_text(s_stats_layer, s_values_buff);
+
+    // Recent history
+    SampleData *sample_data = data_get_sample_data();
+    static char s_history_buff[64];
+    snprintf(
+      s_history_buff,
+      sizeof(s_history_buff),
+      "Recent values:\n%d, %d, %d, %d, %d, %d\nAverage: %d",
+      sample_data->history[0],
+      sample_data->history[1],
+      sample_data->history[2],
+      sample_data->history[3],
+      sample_data->history[4],
+      sample_data->history[5],
+      data_get_history_avg_rate()
+    );
+    text_layer_set_text(s_history_layer, s_history_buff);
+  }
 }
 
 static void window_load(Window *window) {
   Layer *root_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(root_layer);
 
-  GFont sys_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
-  GFont sys_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+  GFont sys_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+  GFont sys_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18);
 
-  // Enable toggle
-  s_enabled_layer = util_make_text_layer(GRect(5, 5, WIDTH, 168), sys_24);
+  s_status_bar = status_bar_layer_create();
+  status_bar_layer_set_separator_mode(s_status_bar, StatusBarLayerSeparatorModeDotted);
+  status_bar_layer_set_colors(s_status_bar, GColorWhite, GColorBlack);
+  layer_add_child(root_layer, status_bar_layer_get_layer(s_status_bar));
+
+  s_enabled_layer = util_make_text_layer(GRect(5, STATUS_BAR_LAYER_HEIGHT, WIDTH - 10, 168), sys_18);
   layer_add_child(root_layer, text_layer_get_layer(s_enabled_layer));
 
-  // Current stats (pending UI)
-  s_stats_layer = util_make_text_layer(GRect(5, 30, WIDTH - 10, 168), sys_18);
+  s_stats_layer = util_make_text_layer(GRect(5, STATUS_BAR_LAYER_HEIGHT + 23, WIDTH - 10, 168), sys_14);
   layer_add_child(root_layer, text_layer_get_layer(s_stats_layer));
 
-  s_history_layer = util_make_text_layer(GRect(5, 100, WIDTH - 10, 168), sys_18);
+  s_history_layer = util_make_text_layer(GRect(5, STATUS_BAR_LAYER_HEIGHT + 100, WIDTH - 10, 168), sys_14);
   layer_add_child(root_layer, text_layer_get_layer(s_history_layer));
-
-  // IF calculated avg rate is 0, show it's still being gathered
-
-  // Average discharge rate
-
-  // Time since last charge
-
-  // Estimated time remaining (days / hours)
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(s_stats_layer);
+  status_bar_layer_destroy(s_status_bar);
+
   text_layer_destroy(s_enabled_layer);
+  text_layer_destroy(s_stats_layer);
   text_layer_destroy(s_history_layer);
 
   window_destroy(s_window);
@@ -104,15 +105,11 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   update_data();
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-}
-
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+// TODO: Move to main.c
 static void battery_handler(BatteryChargeState state) {
   // Un/plugged while app is open
   if (data_get_was_plugged() && !state.is_plugged) {
