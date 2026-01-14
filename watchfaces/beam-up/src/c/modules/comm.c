32b120e62b6cd450eedf2b6aa5a54d833d4e44e7
C-D-Lewis <bonsitm@gmail.com>
2026-01-14 19:40:34 +0000

    Work on beam-up

--- watchfaces/beam-up/src/c/modules/comm.c
+++ watchfaces/beam-up/src/c/modules/comm.c
@@ -1,36 +1,26 @@
 #include "comm.h"
 
-// TODO Use pebble-packet once flint is supported
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
-  Tuple *t = dict_find(iter, DataKeyDate);
-  if (t) data_set_boolean_setting(DataKeyDate, (t->value->int8 == 1));
-
-  t = dict_find(iter, DataKeyAnimations);
-  if (t) data_set_boolean_setting(DataKeyAnimations, (t->value->int8 == 1));
-
-  t = dict_find(iter, DataKeyBTIndicator);
-  if (t) data_set_boolean_setting(DataKeyBTIndicator, (t->value->int8 == 1));
-
-  t = dict_find(iter, DataKeyHourlyVibration);
-  if (t) data_set_boolean_setting(DataKeyHourlyVibration, (t->value->int8 == 1));
-
-  t = dict_find(iter, DataKeySleep);
-  if (t) data_set_boolean_setting(DataKeySleep, (t->value->int8 == 1));
+  // Clay sends all keys at once
+  data_set_boolean_setting(DataKeyDate, packet_get_boolean(iter, DataKeyDate));
+  data_set_boolean_setting(DataKeyAnimations, packet_get_boolean(iter, DataKeyAnimations));
+  data_set_boolean_setting(DataKeyBTIndicator, packet_get_boolean(iter, DataKeyBTIndicator));
+  data_set_boolean_setting(DataKeyHourlyVibration, packet_get_boolean(iter, DataKeyHourlyVibration));
+  data_set_boolean_setting(DataKeySleep, packet_get_boolean(iter, DataKeySleep));
 
 #if defined(PBL_COLOR)
-  t = dict_find(iter, DataKeyForegroundColor);
-  if (t) data_set_foreground_color(GColorFromHEX(t->value->int32));
-
-  t = dict_find(iter, DataKeyBackgroundColor);
-  if (t) data_set_background_color(GColorFromHEX(t->value->int32));
+  data_set_foreground_color(GColorFromHEX(packet_get_integer(iter, DataKeyForegroundColor)));
+  data_set_background_color(GColorFromHEX(packet_get_integer(iter, DataKeyBackgroundColor)));
 #endif
 
   main_window_reload_config(true);
 }
 
 void comm_init(uint32_t inbox, uint32_t outbox) {
+  packet_init();
+
   app_message_register_inbox_received(inbox_received_handler);
   app_message_open(inbox, outbox);
 }
 
-void comm_deinit() { }
+void comm_deinit() {}
