4b5404a50d7a4a72788afb5372ce7f4f4d5b9db8
C-D-Lewis <bonsitm@gmail.com>
2026-01-14 19:16:00 +0000

    Work on thin

--- watchfaces/thin/src/c/modules/comm.c
+++ watchfaces/thin/src/c/modules/comm.c
@@ -3,34 +3,46 @@
 static void in_recv_handler(DictionaryIterator *iter, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Config message received");
 
-  // TODO: Use pebble-packet here
-  Tuple *t = dict_read_first(iter);
-  while (t) {
-    uint32_t key = t->key;
-    if (
-      key == MESSAGE_KEY_EnableDate || key == MESSAGE_KEY_EnableBT ||
-      key == MESSAGE_KEY_EnableBattery || key == MESSAGE_KEY_EnableSecondHand ||
-      key == MESSAGE_KEY_EnableSteps
-    ) {
-      data_set_enable(key, t->value->int32 != 0);
-    } else if (
-      key == MESSAGE_KEY_ColorBackground || key == MESSAGE_KEY_ColorHourMinutes || 
-      key == MESSAGE_KEY_ColorSeconds || key == MESSAGE_KEY_ColorNotches || 
-      key == MESSAGE_KEY_ColorMonthDay || key == MESSAGE_KEY_ColorDate
-    ) {
-      data_set_color(key, GColorFromHEX(t->value->int32));
-    } else {
-      APP_LOG(APP_LOG_LEVEL_WARNING, "Unknown config key received: %d", (int)key);
-    }
-
-    t = dict_read_next(iter);
-  }
+  // Clay sends all keys at once
+  data_set_enable(MESSAGE_KEY_EnableDate, packet_get_boolean(iter, MESSAGE_KEY_EnableDate));
+  data_set_enable(MESSAGE_KEY_EnableBT, packet_get_boolean(iter, MESSAGE_KEY_EnableBT));
+  data_set_enable(MESSAGE_KEY_EnableBattery, packet_get_boolean(iter, MESSAGE_KEY_EnableBattery));
+  data_set_enable(
+    MESSAGE_KEY_EnableSecondHand,
+    packet_get_boolean(iter, MESSAGE_KEY_EnableSecondHand)
+  );
+  data_set_enable(MESSAGE_KEY_EnableSteps, packet_get_boolean(iter, MESSAGE_KEY_EnableSteps));
+  data_set_color(
+    MESSAGE_KEY_ColorBackground,
+    GColorFromHEX(packet_get_integer(iter, MESSAGE_KEY_ColorBackground))
+  );
+  data_set_color(
+    MESSAGE_KEY_ColorHourMinutes,
+    GColorFromHEX(packet_get_integer(iter, MESSAGE_KEY_ColorHourMinutes))
+  );
+  data_set_color(
+    MESSAGE_KEY_ColorSeconds,
+    GColorFromHEX(packet_get_integer(iter, MESSAGE_KEY_ColorSeconds))
+  );
+  data_set_color(
+    MESSAGE_KEY_ColorNotches,
+    GColorFromHEX(packet_get_integer(iter, MESSAGE_KEY_ColorNotches))
+  );
+  data_set_color(
+    MESSAGE_KEY_ColorMonthDay,
+    GColorFromHEX(packet_get_integer(iter, MESSAGE_KEY_ColorMonthDay))
+  );
+  data_set_color(
+    MESSAGE_KEY_ColorDate,
+    GColorFromHEX(packet_get_integer(iter, MESSAGE_KEY_ColorDate))
+  );
 
   main_window_reload_config();
 }
 
 void comm_init() {
-  const int buffer_size = 256;
+  packet_init();
+
   app_message_register_inbox_received(in_recv_handler);
-  app_message_open(buffer_size, buffer_size);
+  app_message_open(256, 256);
 }
