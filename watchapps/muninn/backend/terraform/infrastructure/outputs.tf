6832bb9bb342bb26aba008fce196d9e25c1ececc
C-D-Lewis <bonsitm@gmail.com>
2026-03-08 23:24:09 +0000

    reorg

--- watchapps/muninn/backend/terraform/infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_gateway_url" {
-  value       = aws_apigatewayv2_stage.default.invoke_url
-  description = "The public endpoint for the API GW"
-}
-
-output "domain_name_url" {
-  value       = aws_apigatewayv2_domain_name.domain_name.domain_name
-  description = "API custom domain URL"
-}
--- watchapps/muninn/backend/terraform/outputs.tf
+++ watchapps/muninn/backend/terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "api_gateway_url" {
-  value       = module.main.api_gateway_url
-  description = "The public endpoint for your API"
+  value       = aws_apigatewayv2_stage.default.invoke_url
+  description = "The public endpoint for the API GW"
 }
 
 output "domain_name_url" {
-  value       = module.main.domain_name_url
+  value       = aws_apigatewayv2_domain_name.domain_name.domain_name
   description = "API custom domain URL"
 }
