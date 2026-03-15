e8feaa311c00d511701da69ffdb7d11fea9cc490
C-D-Lewis <Bonsitm@gmail.com>
2026-03-15 12:15:19 +0000

    update

--- watchapps/muninn/backend/lambda/test/index.spec.mjs
+++ watchapps/muninn/backend/lambda/test/index.spec.mjs
@@ -1,11 +1,35 @@
 import { expect } from 'chai';
 import fetch from 'node-fetch';
-import { TEST_UPLOAD } from './constants.mjs';
 
 /** API URL */
 const API_URL = 'https://muninn-api.chrislewis.me.uk';
 /** Test watch token */
 const TEST_WATCH_TOKEN = '0123456789abcdef0123456789abtest';
+/** Test upload body */
+const TEST_UPLOAD = {
+  history: [
+    { ts: 1772798405, cp: 80, r: 8, res: 8 },
+    { ts: 1772776805, cp: 82, r: 8, res: 8 },
+    { ts: 1772755205, cp: 84, r: 8, res: 8 },
+    { ts: 1772733605, cp: 86, r: 8, res: 8 },
+    { ts: 1772712005, cp: 88, r: 8, res: 8 },
+    { ts: 1772690405, cp: 90, r: 8, res: 8 },
+    { ts: 1772668805, cp: 92, r: 8, res: 8 },
+    { ts: 1772647205, cp: 94, r: 8, res: 8 },
+  ],
+  platform: 'TEST',
+  model: 'TEST',
+  firmware: '0.0.0',
+  stats: {
+    count: 0,
+    totalDays: 0,
+    allTimeRate: 0,
+    lastWeekRate: 0,
+    numCharges: 0,
+    mtbc: 0,
+  },
+};
+
 
 let id;
 
@@ -13,19 +37,16 @@ let id;
  * API request helper.
  */
 const apiRequest = async (path, method, body) => {
-  const opts = {
-    method,
-    headers: { 'Content-Type': 'application/json' },
-  };
+  const opts = { method, headers: { 'Content-Type': 'application/json' } };
   if (body) {
     opts. body = JSON.stringify(body);
   }
 
   const res = await fetch(`${API_URL}${path}`, opts);
-  const json = await res.json();
-  const status = res.status;
-
-  return { json, status };
+  return {
+    json: await res.json(),
+    status: res.status,
+  };
 };
 
 describe('API', () => {
@@ -92,7 +113,7 @@ describe('API', () => {
 
       expect(status).to.equal(200);
       expect(json.updatedAt).to.be.greaterThan(Date.now() - 60000);
-      expect(json.history).to.have.length(TEST_UPLOAD.history.length);
+      expect(json.history).to.deep.equal(TEST_UPLOAD.history);
       expect(json.firmware).to.be.equal(TEST_UPLOAD.firmware);
       expect(json.platform).to.be.equal(TEST_UPLOAD.platform);
       expect(json.model).to.be.equal(TEST_UPLOAD.model);
