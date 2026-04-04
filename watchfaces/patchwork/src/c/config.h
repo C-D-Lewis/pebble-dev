a1422bbfc4e181a466d3559d3af6faf41894c78d
C-D-Lewis <bonsitm@gmail.com>
2026-04-04 16:52:58 +0100

    update

--- watchfaces/patchwork/src/c/config.h
+++ watchfaces/patchwork/src/c/config.h
@@ -2,23 +2,12 @@
 
 #include <pebble.h>
 
-// Comment before release!
-// #define TEST
-
 #define PROJECTION_OFFSET GPoint((PS_DISP_W / 2), -24)
 #define B_W 34
-#define B_H 12
+#define B_H 16
 
-#define ORIGIN_X 0
-#define ORIGIN_Y 0
 #define BOX_SIZE GSize(B_W, B_W)
 #define GRID_SIZE 10
 
-// Testing purposes
-#define T_HOURS 18
-#define T_MINUTES 24
-#define T_IS_DAY true
-#define T_IS_CONNECTED false
-
 #define NUM_COLORS 8
-#define Z_MOD 10
+#define Z_MOD 16
--- watchfaces/patchwork/src/c/windows/main_window.c
+++ watchfaces/patchwork/src/c/windows/main_window.c
@@ -14,10 +14,7 @@ static GColor s_palette[] = {
   GColorVividViolet
 };
 
-static AppTimer *s_tap_timer;
-static GFont s_font_80;
-static bool s_tapped = false;
-static time_t s_tap_time;
+static GFont s_font_36, s_font_80;
 
 /*********************************** Drawing **********************************/
 
@@ -26,9 +23,6 @@ static void canvas_layer_update_proc(Layer *layer, GContext *ctx) {
   time_t t = time(NULL);
   tick_time = localtime(&t);
 
-  // Use minute as stable seed
-  srand(tick_time->tm_min);
-
   isometric_begin(ctx);
   isometric_set_projection_offset(PROJECTION_OFFSET);
 
@@ -36,9 +30,6 @@ static void canvas_layer_update_proc(Layer *layer, GContext *ctx) {
   for (int y = 0; y < GRID_SIZE; y++) {
     for (int x = 0; x < GRID_SIZE; x++) {
       GColor color = s_palette[rand() % NUM_COLORS];
-      // z is based on x and y, but also on time since last tap time, trending towards 0
-      // int z_max = rand() % Z_MOD;
-      // int z_now = s_tapped ? z_max : (z_max * (time(NULL) - s_tap_time)) / 2;
       int z_now = rand() % Z_MOD;
       drawing_draw_square(Vec3(x * B_W, y * B_W, z_now), color);
     }
@@ -50,14 +41,14 @@ static void canvas_layer_update_proc(Layer *layer, GContext *ctx) {
   static char s_time_buff[6];
   strftime(s_time_buff, sizeof(s_time_buff), "%H:%M", tick_time);
   graphics_context_set_text_color(ctx, GColorBlack);
-  int x = 0;
-  int y = scl_y(240);
+  int x = scl_x_pp({.e = 25, .g = 20});
+  int y = scl_y_pp({.e = 180, .g = 210});
   graphics_draw_text(
     ctx,
     s_time_buff,
     s_font_80,
-    GRect(x, y, PS_DISP_W, PS_DISP_H),
-    GTextOverflowModeFill,
+    GRect(x - 3, y - 3, PS_DISP_W, PS_DISP_H),
+    GTextOverflowModeWordWrap,
     GTextAlignmentCenter,
     NULL
   );
@@ -66,33 +57,45 @@ static void canvas_layer_update_proc(Layer *layer, GContext *ctx) {
     ctx,
     s_time_buff,
     s_font_80,
-    GRect(x + 3, y + 3, PS_DISP_W, PS_DISP_H),
-    GTextOverflowModeFill,
+    GRect(x, y, PS_DISP_W, PS_DISP_H),
+    GTextOverflowModeWordWrap,
+    GTextAlignmentCenter,
+    NULL
+  );
+
+  // Date
+  static char s_date_buff[20];
+  strftime(s_date_buff, sizeof(s_date_buff), "%e %B %Y", tick_time);
+
+  x = scl_x_pp({.e = -5, .g = -5});
+  y = scl_y_pp({.e = 550, .g = 530});
+  graphics_context_set_text_color(ctx, GColorBlack);
+  graphics_draw_text(
+    ctx,
+    s_date_buff,
+    s_font_36,
+    GRect(x - 2, y - 2, PS_DISP_W, PS_DISP_H),
+    GTextOverflowModeWordWrap,
+    GTextAlignmentCenter,
+    NULL
+  );
+  graphics_context_set_text_color(ctx, GColorWhite);
+  graphics_draw_text(
+    ctx,
+    s_date_buff,
+    s_font_36,
+    GRect(x, y, PS_DISP_W, PS_DISP_H),
+    GTextOverflowModeWordWrap,
     GTextAlignmentCenter,
     NULL
   );
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(s_canvas_layer);
-}
+  // Use minute as stable seed
+  srand(tick_time->tm_hour + tick_time->tm_min);
 
-static void timer_handler(void *data) {
   layer_mark_dirty(s_canvas_layer);
-}
-
-static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
-  // Prevent multiple taps in a short time
-  time_t now = time(NULL);
-  if (now - s_tap_time < 5) return;
-  
-  // Move now
-  s_tapped = true;
-  s_tap_time = now;
-  layer_mark_dirty(s_canvas_layer);
-
-  // Delay easing back to normal
-  s_tap_timer = app_timer_register(2000, (AppTimerCallback)timer_handler, NULL);
 }
 
 /************************************ Window **********************************/
@@ -101,6 +104,7 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  s_font_36 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FORCED_SQUARE_36));
   s_font_80 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FORCED_SQUARE_80));
 
   s_canvas_layer = layer_create(bounds);
@@ -127,5 +131,4 @@ void main_window_push() {
   window_stack_push(s_window, true);
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  accel_tap_service_subscribe(accel_tap_handler);
 }
